442a144c30012620fe9758c8a1f301cce185a449
Anhelina <avelichko469@gmail.com>
2025-06-24 19:56:13 +0300

    build project

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/GalleryWebsite/',      // ensure all asset URLs are /GalleryWebsite/…
   plugins: [react()],
   build: {
-    outDir: 'docs'               // ← build into docs/ instead of dist/
+    outDir: 'docs'
   }
-});
+})
