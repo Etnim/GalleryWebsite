b71aad5b100c81c808b47e5e7b43192abd7e94bd
Anhelina <avelichko469@gmail.com>
2025-06-24 18:46:04 +0300

    Add styles for header

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Your website name</p>
+            <div>Icons made from <a href="https://www.onlinewebfonts.com/icon">svg icons</a>is licensed by CC BY 4.0</div>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,15 +3,15 @@ import styles from "./Header.module.css";
 function Header() {
     return (
         <header>
-            <h1 className={styles.h1}>My website</h1>
             <nav className={styles.navbar}>
+                <a href="#home">Logo</a>
                 <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#contact">CONTACT</a></li>
+                    <li><a href="#about">ABOUT</a></li>
+                    <li><a href="#about">SOACIALS</a></li>
+
                 </ul>
             </nav>
-            <hr></hr>
         </header>
     );
 }
