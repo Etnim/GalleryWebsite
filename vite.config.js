429b3aec5eebb9c6ced10bbe86b43a14aa553999
Anhelina <avelichko469@gmail.com>
2025-06-24 19:17:32 +0300

    changed output directory

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/GalleryWebsite/',      // ensure all asset URLs are /GalleryWebsite/…
   plugins: [react()],
-})
+  build: {
+    outDir: 'docs'               // ← build into docs/ instead of dist/
+  }
+});
