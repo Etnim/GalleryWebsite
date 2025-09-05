7a36df7bb0783ef771c7508b693e03dc9c61b9ee
Anhelina <avelichko469@gmail.com>
2025-09-05 18:46:53 +0300

    Exctract common attributes for media

--- src/Gallery/DesignCard/DesignCard.jsx
+++ src/Gallery/DesignCard/DesignCard.jsx
@@ -1,29 +1,28 @@
 import PropTypes from 'prop-types';
 import TagList from './TagList';
 import styles from './DesignCard.module.css';
-
-const DEFAULT_PRELOAD = 'metadata';
-const DEFAULT_LOUDING = 'lazy';
+import videoAttributes from '../../content/default-attributes/video.json';
+import imageAttributes from '../../content/default-attributes/image.json';
 
 export default function DesignCard({
     cardName = 'Empty Name',
     listOfTags = [],
-    media = { type: 'image', src: '' },
+    media = { type: 'image'},
 }) {
     return (
         <div className={styles.card}>
             {media.type === 'video' ? (
                 <video
                     className={styles.media}
-                    poster={media.poster || undefined}
-                    autoPlay={!!media.autoPlay}
-                    muted={!!media.muted}
-                    loop={!!media.loop}
-                    playsInline={media.playsInline !== false}
-                    controls={!!media.controls}
-                    preload={media.preload || 'metadata'}
+                    poster={videoAttributes.poster || media.poster}
+                    autoPlay={videoAttributes.autoPlay || media.autoPlay}
+                    muted={videoAttributes.muted || media.muted}
+                    loop={videoAttributes.loop || media.loop}
+                    playsInline={videoAttributes.playsInline || media.playsInline}
+                    controls={videoAttributes.controls || media.controls}
+                    preload={videoAttributes.preload || media.preload}
                 >
-                    {media.src && <source src={media.src} />}
+                    {media.src && <source src={videoAttributes.src || media.src} />}
                     {media.sources?.map(s => <source key={s.src} src={s.src} type={s.type} />)}
                     Sorry, your browser doesn’t support embedded videos.
                 </video>
@@ -31,9 +30,9 @@ export default function DesignCard({
                 media?.src && (
                     <img
                         className={styles.media}
-                        src={media.src}
-                        alt={media.alt || `Image of ${cardName}`}
-                        loading="lazy"
+                        src={imageAttributes.src || media.src}
+                        alt={imageAttributes.src || media.alt}
+                        loading={imageAttributes.loading || media.loading || 'lazy'}
                     />
                 )
             }
@@ -49,7 +48,6 @@ DesignCard.propTypes = {
     listOfTags: PropTypes.arrayOf(PropTypes.string),
     media: PropTypes.shape({
         type: PropTypes.oneOf(['image', 'video']).isRequired,
-        poster: PropTypes.string,
         src: PropTypes.string,
     }),
 };
