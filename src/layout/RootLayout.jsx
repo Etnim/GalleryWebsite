d602301d673e05dc266fde462881a88ec70251db
Anhelina <avelichko469@gmail.com>
2025-12-08 14:17:36 +0200

    Add content to about page

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,5 +1,6 @@
 import Header from '../components/Header/Header';
 import { Outlet } from 'react-router-dom';
+import ScrollToTop from '../components/ScrollToTop.jsx';
 
 const RootLayout = () => {
     return (
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,16 @@
+import styles from '../styles/AboutPage.module.css';
+import drawing from '../assets/about-page-drawing.jpg';
+
 export default function About(){
-    return <div>About Page</div>;
+    return <div className={styles.aboutContainer}>
+        <h1 className={styles.title}>About</h1>
+        <div className={styles.textContainer}>
+            <p>Usually this section is meant to describe my professional attitude to design, main inspirations that led me to this path and refined methods of work. Although it might be appropriate for purely “business based” communication, I find it extremely boring.</p>
+            <p>For all the business-related inquires I prefer my works to talk for me, about my design approach and skills.</p>
+            <p>And if you still continue reading this then may God help you as I am about to bring a personal touch to this presentation. I love traveling and asking question “why?” to discover the depths of out cosmic existence. I find drinking alcohol inefficient to be called a party drug. So I prefer a stimulant drug caffeine, specifically in a form of traditional south American drink - Yerba Mate. Occasionally I might attend judo/jiu jitsu training sessions to tease my ego with wins against novices. I dream of the times when I will be a renowned designer and be able to put a 10k euros price tag for a logo design which would take me 2 weeks to make, while still having clients waiting in queue for my design services.</p>
+            <p>But for now I am just a guy who loves moving pixels around to create meaning  ;)</p>
+        </div>
+        <img src={drawing} className={styles.aboutPageDrawing}/>
+    </div>;
+
 }
