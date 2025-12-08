d3337b907c210b6b25022dc5cf41bc5286717ff6
Anhelina <avelichko469@gmail.com>
2025-12-08 13:22:34 +0200

    Remove footer

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,13 +1,11 @@
 import Header from '../components/Header/Header';
 import { Outlet } from 'react-router-dom';
-import Footer from '../components/Footer/Footer';
 
 const RootLayout = () => {
     return (
         <div>
             <Header />
             <Outlet />
-            <Footer />
         </div>
     );
 }
