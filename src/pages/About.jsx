3797624a0e304ee2d3662b6a5c39c9b8e4f2e2df
Anhelina <avelichko469@gmail.com>
2025-12-09 15:08:43 +0200

    fix position of divs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import styles from '../styles/AboutContactPages.module.css';
 import drawing from '../assets/about-page-drawing.jpg';
 
-export default function About(){
+export default function About() {
     return <div className={styles.pageContainer}>
         <h1 className={styles.title}>About</h1>
         <div className={styles.textContainer}>
@@ -10,7 +10,10 @@ export default function About(){
             <p>And if you still continue reading this then may God help you as I am about to bring a personal touch to this presentation. I love traveling and asking question “why?” to discover the depths of out cosmic existence. I find drinking alcohol inefficient to be called a party drug. So I prefer a stimulant drug caffeine, specifically in a form of traditional south American drink - Yerba Mate. Occasionally I might attend judo/jiu jitsu training sessions to tease my ego with wins against novices. I dream of the times when I will be a renowned designer and be able to put a 10k euros price tag for a logo design which would take me 2 weeks to make, while still having clients waiting in queue for my design services.</p>
             <p>But for now I am just a guy who loves moving pixels around to create meaning  ;)</p>
         </div>
-        <img src={drawing} className={styles.aboutPageDrawing}/>
+        <img src={drawing} className={styles.aboutPageDrawing} />
+        <p id={styles.comment}><i>If you wonder how I look like, this is how my girlfriend sees me,
+            by the way, <a href="https://www.linkedin.com/in/anhelina-velychko-56047a240/" target="_blank"><b>she</b></a> is the one who build this website</i>
+        </p>
     </div>;
 
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,18 @@
 import styles from '../styles/AboutContactPages.module.css';
 import contactImage from '../assets/ContactPage.gif';
 import linkedIn from "../../public/LinkedInicon.png";
-import { Link } from 'react-router-dom';
 
-
-export default function Contact(){
+export default function Contact() {
     return <div className={styles.pageContainer}>
         <h1 className={styles.title}>Contact Page</h1>
         <div className={styles.twoColumnLayout}>
-        <ul className={styles.leftColumn}>
-            <li>Ignat Voronovich</li>
-            <li>Address: Vilnius, Lithuania</li>
-            <li>Email: hol1.visualcraft@gmaul.com</li>
-            <li><Link to="https://www.linkedin.com/in/ignat-voronovich-313951241/"><img src={linkedIn} className={styles.icon}></img>: linked in</Link></li>
-        </ul>
-        <img src={contactImage} className={styles.rightColumn}></img>
+            <ul className={styles.leftColumn}>
+                <li>Ignat Voronovich</li>
+                <li>Address: Vilnius, Lithuania</li>
+                <li>Email: hol1.visualcraft@gmaul.com</li>
+                <li><a href="https://www.linkedin.com/in/ignat-voronovich-313951241/" target="_blank"><img src={linkedIn} className={styles.icon}></img>: linked in</a></li>
+            </ul>
+            <img src={contactImage} className={styles.rightColumn}></img>
         </div>
     </div>;
 }
