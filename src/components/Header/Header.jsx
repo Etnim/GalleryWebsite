3554d0963fb5f219639b2a256f4dc0068784de6c
Anhelina <avelichko469@gmail.com>
2025-12-09 14:47:03 +0200

    Add contact info

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import logo from "../../../public/WhiteLogo.png";
 
 function Header() {
     return (
         <header>
             <nav className={styles.navbar}>
-                <Link to="/GalleryWebsite/"><img id={styles.logo} src={"WhiteLogo.png"} /></Link>
+                <Link to="/GalleryWebsite/"><img id={styles.logo} src={logo} /></Link>
                 <ul>
                     <Link to="/GalleryWebsite/contact/"><li>CONTACT</li></Link>
                     <Link to="/GalleryWebsite/about/"><li>ABOUT</li></Link>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,20 @@
+import styles from '../styles/AboutContactPages.module.css';
+import contactImage from '../assets/ContactPage.gif';
+import linkedIn from "../../public/LinkedInicon.png";
+import { Link } from 'react-router-dom';
+
+
 export default function Contact(){
-    return <div>
-        <h1>Contact Page</h1>
+    return <div className={styles.pageContainer}>
+        <h1 className={styles.title}>Contact Page</h1>
+        <div className={styles.twoColumnLayout}>
+        <ul className={styles.leftColumn}>
+            <li>Ignat Voronovich</li>
+            <li>Address: Vilnius, Lithuania</li>
+            <li>Email: hol1.visualcraft@gmaul.com</li>
+            <li><Link to="https://www.linkedin.com/in/ignat-voronovich-313951241/"><img src={linkedIn} className={styles.icon}></img>: linked in</Link></li>
+        </ul>
+        <img src={contactImage} className={styles.rightColumn}></img>
+        </div>
     </div>;
 }
