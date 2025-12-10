301d70d1f461f01a7466feee32b8b182fdc745a9
Anhelina <avelichko469@gmail.com>
2025-12-10 16:52:05 +0200

    add footer to gallery + fix margin in about page

---
+++ src/components/WorksGallery/FooterGallery.jsx
@@ -0,0 +1,12 @@
+import styles from "./WorksGallery.module.css";
+
+function FooterGallery() {
+    return (
+        <footer className={styles.footerGallery}>
+           <p><i>@Developed by <a href="https://www.linkedin.com/in/anhelina-velychko-56047a240/" target="_blank"><b>Etnim</b></a></i>
+            </p>
+        </footer>
+    );
+}
+
+export default FooterGallery;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import WorksGallery from '../components/WorksGallery/WorksGallery.jsx';
 import {getAllProjects} from '../services/projects-data-service.js';
+import FooterGallery from '../components/WorksGallery/FooterGallery.jsx';
 
 export default function Home() {
   return (
     <>
       <WorksGallery listOfCards={getAllProjects()} />
+      <FooterGallery />
     </>
   );
 } 
