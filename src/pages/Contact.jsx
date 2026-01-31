ba0dbe52d0ccb2306efa6f49e690d2e0feb9768e
Anhelina <avelichko469@gmail.com>
2026-01-31 15:32:37 +0200

    Change Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,24 @@
 import styles from '../styles/AboutContactPages.module.css';
-import contactImage from '../assets/contactPage.gif';
+import contactImage from '../assets/hands.gif';
 import linkedIn from "../../public/LinkedInicon.png";
+import gmail from "../../public/GmailIcon.png";
+
 
 export default function Contact() {
     return <div className={styles.pageContainer}>
-        <h1 className={styles.title}>Contacts</h1>
+        <h1 className={styles.title}>Contact</h1>
         <div className={styles.twoColumnLayout}>
-            <ul className={styles.leftColumn}>
-                <li>Ignat Voronovich</li>
-                <li>Address: Vilnius, Lithuania</li>
-                <li>Email: hol1.visualcraft@gmaul.com</li>
-                <li><a href="https://www.linkedin.com/in/ignat-voronovich-313951241/" target="_blank"><img src={linkedIn} className={styles.icon}></img>: linked in</a></li>
-            </ul>
+            <div className={styles.leftColumn}>
+                <p id={styles.name}>Ignat Voronovich</p>
+                <p>Vilnius, Lithuania</p>
+                <ul className={styles.contactIcons}>
+                    <li><a href="https://mail.google.com/mail/?view=cm&fs=1&to=hol1.visualcraft@gmail.com"
+                        target="_blank">
+                            <img src={gmail} className={styles.icon} />
+                    </a></li>
+                    <li><a href="https://www.linkedin.com/in/ignat-voronovich-313951241/" target="_blank"><img src={linkedIn} className={styles.icon}></img></a></li>
+                </ul>
+            </div>
             <img src={contactImage} className={styles.rightColumn}></img>
         </div>
     </div>;
