6b0a95922149a3c4ff4aacbe389704762bfbbd2d
Holy <ho1y.visualcraft@gmail.com>
2026-07-06 22:31:29 +0300

    little typo fixes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../styles/AboutPage.module.css";
-import drawing from "../assets/about-page-drawing.jpg";
+import drawing from "../assets/Cover.png";
 import useLenisScroll from "../hooks/useLenisScroll";
 
 export default function About() {
@@ -12,20 +12,16 @@ export default function About() {
       <div className={styles.infoContainer}>
         <div className={styles.textColumn}>
           <div className={styles.textBlock}>
-            <p className={styles.subtitle}>Who I am</p>
+            <p className={styles.subtitle}>Background</p>
             <p>
-              My name is Ignat, and I am a passionate web developer with a love
-              for creating beautiful and functional websites. I have been
-              working in the field for several years, and I have experience with
-              a wide range of technologies and frameworks.
+              Hi, I’m Ignat - product designer in Vilnius.
+4 years as a graphic designer, specializing in visual identities. In 2026 decided to go deeper and switched to mastering product design.
             </p>
           </div>
           <div className={styles.textBlock}>
-            <p className={styles.subtitle}>What I do</p>
+            <p className={styles.subtitle}>Specialization</p>
             <p>
-              I am always looking for new challenges and opportunities to learn
-              and grow as a developer. If you are interested in working with me,
-              please feel free to reach out to me through the contact page.
+              My current specialty is developing intuitive and clean user interfaces across mobile and web applications. Previous experience of working with graphics makes me a versatile soldier, capable of adapting to any project.
             </p>
           </div>
         </div>
