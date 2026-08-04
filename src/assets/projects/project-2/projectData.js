53cc086719ebe52b5692b43c52e04474135b2b72
Holy <ho1y.visualcraft@gmail.com>
2026-08-04 14:08:27 +0300

    Added 9 new images, removed old ones,added new descriptions ,fully finished project case

--- src/assets/projects/project-2/projectData.js
+++ src/assets/projects/project-2/projectData.js
@@ -6,7 +6,7 @@ export const projectDetails = {
   type: "Mobile Application",
   client: "Nature Research Centre, Lithuania",
   role: ["Product Designer"],
-  status: "In development",
+  status: "Handed off · Building",
   year: "2026",
   template: [
     {
@@ -45,14 +45,14 @@ export const projectDetails = {
       ]
     },
     {
-      file: "Rock Sense Collage Brown.png",
+      file: "3 Main Screens.png",
     },
     {
       array: [
         {
           textSection: {
-            title: "Field Tools",
-            file: "designsystem.txt",
+            title: "Rock Identification",
+            file: "rock identification.txt",
           }
         },
         {
@@ -61,8 +61,106 @@ export const projectDetails = {
       ]
     },
     {
-      file: "Rock Sense Collage Brown 2.png",
+      file: "Rock Identification.png",
     },
-
+{
+      array: [
+        {
+          textSection: {
+            title: "Analysis Result",
+            file: "analysis result.txt",
+          }
+        },
+        {
+          textSection: null
+      }
+      ]
+    },
+    {
+      file: "Analysis Result - Description.png",
+    },
+    {
+      file: "Analysis Result - Value.png",
+    },
+    {
+      array: [
+        {
+          textSection: {
+            title: "Premium",
+            file: "premium.txt",
+          }
+        },
+        {
+          textSection: null
+      }
+      ]
+    },
+    {
+      file: "Premium.png",
+    },
+    {
+      array: [
+        {
+          textSection: {
+            title: "Detector and Statistics",
+            file: "detector and statistics.txt",
+          }
+        },
+        {
+          textSection: null
+      }
+      ]
+    },
+    {
+      file: "Detector and Statistics.png",
+    },
+    {
+      array: [
+        {
+          textSection: {
+            title: "Collection",
+            file: "collection.txt",
+          }
+        },
+        {
+          textSection: null
+      }
+      ]
+    },
+    {
+      file: "Collection.png",
+    },
+    {
+      array: [
+        {
+          textSection: {
+            title: "History",
+            file: "history.txt",
+          }
+        },
+        {
+          textSection: null
+      }
+      ]
+    },
+    {
+      file: "History.png",
+    },
+    {
+      array: [
+        {
+          textSection: {
+            title: "Icon Set",
+            file: "icon set.txt",
+          }
+        },
+        {
+          textSection: null
+      }
+      ]
+    },
+    {
+      file: "Icon Set.png",
+    },
   ],
 };
